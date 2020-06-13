58a299d20dccc73661c82e970325209084767eb2
takuto222 <takuto.ft222@gmail.com>
2020-06-13 13:58:46 +0900

    Add Policy

--- resources/views/articles/article.blade.php
+++ resources/views/articles/article.blade.php
@@ -2,7 +2,7 @@
   <svg class="bd-placeholder-img mr-3" width="64" height="64" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 64x64"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"/><text x="50%" y="50%" fill="#dee2e6" dy=".3em">64x64</text></svg>
   <div class="media-body">
     <h2 class="article-title mt-3 mb-2">{{ $article->title }}</h2>
-    <p>投稿者：{{  Auth::user()->name }}</p>
+    <p>投稿者：{{ $article->user->name }}</p>
   </div>
 </div>
 
